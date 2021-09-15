cdfd6b789766d18f50cfdcbb9e050f84b5346105
Heather Smith <halexsmith86@gmail.com>
2021-09-15 10:18:20 -0400

    input border turns red when invalid email entered

--- script.js
+++ script.js
@@ -4,11 +4,16 @@ function checkEmail(value) {
     var value = $('input[name=email]').val();
     // regex
     var re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-    // if value is not an email address, unhide the red error text
+    // if value is not an email address...
     if(!re.test(value)){
         var errorMsg = document.querySelector('.error');
         var exPoint = document.querySelector('.exclamation');
+        var input = document.querySelector('input');
+        // unhide the error message
         errorMsg.style.display = "block";
+        // unhide the exclamation point
         exPoint.style.display = "block";
+        // turn the border around the input field red
+        input.style.border = "2px solid #F96464";
     }
 }
